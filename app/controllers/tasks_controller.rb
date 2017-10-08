101865609bfaab28e8a15d0a36c3a864a48ed47b
Kazuhiro Iwata <biancci@gmail.com>
2017-10-08 05:06:21 +0000

    CRUD for only the user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -13,14 +15,15 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に登録されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'タスクが登録されませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
   
@@ -43,10 +46,17 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
   
   #タスク登録を共通化
   def set_task
@@ -55,7 +65,7 @@ class TasksController < ApplicationController
   
   #Strong Parameter
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status, :user)
   end
   
 end
